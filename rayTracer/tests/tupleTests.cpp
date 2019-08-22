46859820eb07c0a367b7ecebaa0a083fc8ca8da1
Ilia <ilia.shibaev@gmail.com>
2019-08-22 10:41:35 +1000

    initial commit for raytracer

---
+++ rayTracer/tests/tupleTests.cpp
@@ -0,0 +1,17 @@
+#include "../src/tuple.h"
+#include "testUtils.h"
+#include <iostream>
+
+bool testVec() {
+    glm::vec3 testVec(3.f,4.f,5.f);
+
+    std::cout << "Tuple x: " << testVec.x << std::endl;
+
+    return true;
+}
+
+int main(int argc, char const *argv[])
+{
+    ASSERT_THROW(testVec());
+    return 0;
+}
