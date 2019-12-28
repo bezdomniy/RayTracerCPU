74d14d3cd1cd1fc47c40c9a34a743101932f8af6
Ilia <ilia.shibaev@gmail.com>
2019-12-28 18:19:23 +1100

    set up for compile to webassembly

--- rayTracer/src/main.cpp
+++ rayTracer/src/main.cpp
@@ -11,14 +11,26 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <iostream>
 
+#ifdef __EMSCRIPTEN__
+#include <emscripten/val.h>
+#endif
+
 int main(int argc, char const *argv[])
 {
     World world;
     std::shared_ptr<Camera> camera = world.loadFromFile("./src/scene2.yaml");
+    // std::shared_ptr<Camera> camera = world.loadFromFile("scene2.yaml");
 
     Renderer renderer(camera);
     renderer.render(world);
     renderer.canvas.writeToPPM("./test.ppm", false);
 
+    #ifdef __EMSCRIPTEN__
+    emscripten::val::global("window").call<void>(
+        "offerFileAsDownload",
+        std::string("test.ppm"),
+        std::string("mime/type")
+  );
+    #endif
     return 0;
 }
