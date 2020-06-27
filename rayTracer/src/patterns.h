19d3e79cc5e59683509412b44e6855cee65e3d8f
bezdomniy <ilia.shibaev@gmail.com>
2020-06-27 22:28:39 +1000

    remove sdl uv textures - need to reimplement without sdl

--- rayTracer/src/patterns.h
+++ rayTracer/src/patterns.h
@@ -3,40 +3,45 @@
 #include "../include/SimplexNoise.h"
 #include "pattern.h"
 #include "textureMap.h"
-#include "uvTextures.h"
+// #include "uvTextures.h"
 #include <glm/gtc/matrix_inverse.hpp>
 #include <memory>
 #include <vector>
 
-class StripedPattern : public ColourPattern {
+class StripedPattern : public ColourPattern
+{
 public:
   StripedPattern(glm::dvec3 colourA, glm::dvec3 colourB);
   ~StripedPattern();
   virtual glm::dvec3 patternAt(glm::dvec4 point) override;
 };
 
-class GradientPattern : public ColourPattern {
+class GradientPattern : public ColourPattern
+{
 public:
   GradientPattern(glm::dvec3 colourA, glm::dvec3 colourB);
   ~GradientPattern();
   virtual glm::dvec3 patternAt(glm::dvec4 point) override;
 };
 
-class RingPattern : public ColourPattern {
+class RingPattern : public ColourPattern
+{
 public:
   RingPattern(glm::dvec3 colourA, glm::dvec3 colourB);
   ~RingPattern();
   virtual glm::dvec3 patternAt(glm::dvec4 point) override;
 };
 
-class CheckedPattern : public ColourPattern {
+class CheckedPattern : public ColourPattern
+{
 public:
   CheckedPattern(glm::dvec3 colourA, glm::dvec3 colourB);
   ~CheckedPattern();
   virtual glm::dvec3 patternAt(glm::dvec4 point) override;
 };
 
-class BlendedPattern : public Pattern {
+class BlendedPattern : public Pattern
+{
 public:
   BlendedPattern(std::shared_ptr<Pattern> &patternA,
                  std::shared_ptr<Pattern> &patternB);
@@ -49,7 +54,8 @@ public:
   std::shared_ptr<Pattern> patternB;
 };
 
-class PerturbedPattern : public Pattern {
+class PerturbedPattern : public Pattern
+{
 public:
   PerturbedPattern(std::shared_ptr<Pattern> &pattern, double perturbedCoeff);
   PerturbedPattern(const PerturbedPattern &perturbedPattern);
@@ -61,14 +67,14 @@ public:
   double perturbedCoeff;
 };
 
-class MappedPattern : public Pattern {
-public:
-  MappedPattern(std::shared_ptr<UVTexture> &uvTexture, std::shared_ptr<TextureMap> &textureMap);
-  MappedPattern(const MappedPattern &mappedPattern);
-  ~MappedPattern();
+// class MappedPattern : public Pattern {
+// public:
+//   MappedPattern(std::shared_ptr<UVTexture> &uvTexture, std::shared_ptr<TextureMap> &textureMap);
+//   MappedPattern(const MappedPattern &mappedPattern);
+//   ~MappedPattern();
 
-  virtual glm::dvec3 patternAt(glm::dvec4 point) override;
+//   virtual glm::dvec3 patternAt(glm::dvec4 point) override;
 
-  std::shared_ptr<UVTexture> uvTexture;
-  std::shared_ptr<TextureMap> textureMap;
-};
+//   std::shared_ptr<UVTexture> uvTexture;
+//   std::shared_ptr<TextureMap> textureMap;
+// };
--- rayTracer/src/uvTextures.cpp
+++ rayTracer/src/uvTextures.cpp
@@ -1,165 +1,165 @@
-#include "uvTextures.h"
+// #include "uvTextures.h"
 
-UVTexture::UVTexture(/* args */)
-{
-}
+// UVTexture::UVTexture(/* args */)
+// {
+// }
 
-UVTexture::~UVTexture()
-{
-}
+// UVTexture::~UVTexture()
+// {
+// }
 
-CheckeredTexture::CheckeredTexture()
-{
-}
+// CheckeredTexture::CheckeredTexture()
+// {
+// }
 
-CheckeredTexture::CheckeredTexture(glm::dvec3 colourA, glm::dvec3 colourB, int width, int height)
-{
+// CheckeredTexture::CheckeredTexture(glm::dvec3 colourA, glm::dvec3 colourB, int width, int height)
+// {
 
-  this->colourA = colourA;
-  this->colourB = colourB;
-  this->width = width;
-  this->height = height;
-}
+//   this->colourA = colourA;
+//   this->colourB = colourB;
+//   this->width = width;
+//   this->height = height;
+// }
 
-CheckeredTexture::~CheckeredTexture()
-{
-}
+// CheckeredTexture::~CheckeredTexture()
+// {
+// }
 
-glm::dvec3 CheckeredTexture::patternAt(glm::dvec2 uv, int faceIndex)
-{
-  int u2 = (int)std::floor(uv.x * this->width);
-  int v2 = (int)std::floor(uv.y * this->height);
+// glm::dvec3 CheckeredTexture::patternAt(glm::dvec2 uv, int faceIndex)
+// {
+//   int u2 = (int)std::floor(uv.x * this->width);
+//   int v2 = (int)std::floor(uv.y * this->height);
 
-  if ((u2 + v2) % 2 == 0)
-  {
-    return this->colourA;
-  }
-  else
-  {
-    return this->colourB;
-  }
-}
+//   if ((u2 + v2) % 2 == 0)
+//   {
+//     return this->colourA;
+//   }
+//   else
+//   {
+//     return this->colourB;
+//   }
+// }
 
-void CheckeredTexture::loadRight(std::string const &path)
-{
-}
+// void CheckeredTexture::loadRight(std::string const &path)
+// {
+// }
 
-void CheckeredTexture::loadLeft(std::string const &path)
-{
-}
+// void CheckeredTexture::loadLeft(std::string const &path)
+// {
+// }
 
-void CheckeredTexture::loadUp(std::string const &path)
-{
-}
+// void CheckeredTexture::loadUp(std::string const &path)
+// {
+// }
 
-void CheckeredTexture::loadDown(std::string const &path)
-{
-}
+// void CheckeredTexture::loadDown(std::string const &path)
+// {
+// }
 
-void CheckeredTexture::loadFront(std::string const &path)
-{
-}
+// void CheckeredTexture::loadFront(std::string const &path)
+// {
+// }
 
-void CheckeredTexture::loadBack(std::string const &path)
-{
-}
+// void CheckeredTexture::loadBack(std::string const &path)
+// {
+// }
 
-ImageTexture::ImageTexture()
-{
-  this->textures.resize(6);
-}
+// ImageTexture::ImageTexture()
+// {
+//   this->textures.resize(6);
+// }
 
-ImageTexture::ImageTexture(std::string const &path)
-{
-  this->textures.push_back(IMG_Load(path.c_str()));
-}
+// ImageTexture::ImageTexture(std::string const &path)
+// {
+//   this->textures.push_back(IMG_Load(path.c_str()));
+// }
 
-ImageTexture::~ImageTexture()
-{
-}
+// ImageTexture::~ImageTexture()
+// {
+// }
 
-Uint32 ImageTexture::pixelFromSurface(SDL_Surface *surface, int x, int y)
-{
-  int bpp = surface->format->BytesPerPixel;
-  /* Here p is the address to the pixel we want to retrieve */
-  Uint8 *p = (Uint8 *)surface->pixels + y * surface->pitch + x * bpp;
+// Uint32 ImageTexture::pixelFromSurface(SDL_Surface *surface, int x, int y)
+// {
+//   int bpp = surface->format->BytesPerPixel;
+//   /* Here p is the address to the pixel we want to retrieve */
+//   Uint8 *p = (Uint8 *)surface->pixels + y * surface->pitch + x * bpp;
 
-  switch (bpp)
-  {
-  case 1:
-    return *p;
-    break;
+//   switch (bpp)
+//   {
+//   case 1:
+//     return *p;
+//     break;
 
-  case 2:
-    return *(Uint16 *)p;
-    break;
+//   case 2:
+//     return *(Uint16 *)p;
+//     break;
 
-  case 3:
-    if (SDL_BYTEORDER == SDL_BIG_ENDIAN)
-      return p[0] << 16 | p[1] << 8 | p[2];
-    else
-      return p[0] | p[1] << 8 | p[2] << 16;
-    break;
+//   case 3:
+//     if (SDL_BYTEORDER == SDL_BIG_ENDIAN)
+//       return p[0] << 16 | p[1] << 8 | p[2];
+//     else
+//       return p[0] | p[1] << 8 | p[2] << 16;
+//     break;
 
-  case 4:
-    return *(Uint32 *)p;
-    break;
+//   case 4:
+//     return *(Uint32 *)p;
+//     break;
 
-  default:
-    return 0; /* shouldn't happen, but avoids warnings */
-  }
-}
+//   default:
+//     return 0; /* shouldn't happen, but avoids warnings */
+//   }
+// }
 
-glm::dvec3 ImageTexture::rgbFromPixel(SDL_Surface *surface, int x, int y)
-{
-  SDL_Color rgb;
-  Uint32 pixel = pixelFromSurface(surface, x, y);
-  SDL_GetRGB(pixel, surface->format, &rgb.r, &rgb.g, &rgb.b);
+// glm::dvec3 ImageTexture::rgbFromPixel(SDL_Surface *surface, int x, int y)
+// {
+//   SDL_Color rgb;
+//   Uint32 pixel = pixelFromSurface(surface, x, y);
+//   SDL_GetRGB(pixel, surface->format, &rgb.r, &rgb.g, &rgb.b);
 
-  double r = (rgb.r / 255.);
-  double g = (rgb.g / 255.);
-  double b = (rgb.b / 255.);
+//   double r = (rgb.r / 255.);
+//   double g = (rgb.g / 255.);
+//   double b = (rgb.b / 255.);
 
-  return glm::dvec3(r, g, b);
-}
+//   return glm::dvec3(r, g, b);
+// }
 
-glm::dvec3 ImageTexture::patternAt(glm::dvec2 uv, int faceIndex)
-{
-  double u = uv.x;
-  double v = 1. - uv.y;
+// glm::dvec3 ImageTexture::patternAt(glm::dvec2 uv, int faceIndex)
+// {
+//   double u = uv.x;
+//   double v = 1. - uv.y;
 
-  double x = u * (this->textures.at(faceIndex)->w - 1);
-  double y = v * (this->textures.at(faceIndex)->h - 1);
+//   double x = u * (this->textures.at(faceIndex)->w - 1);
+//   double y = v * (this->textures.at(faceIndex)->h - 1);
 
-  return rgbFromPixel(this->textures.at(faceIndex), (int)std::round(x), (int)std::round(y));
-}
+//   return rgbFromPixel(this->textures.at(faceIndex), (int)std::round(x), (int)std::round(y));
+// }
 
-void ImageTexture::loadRight(std::string const &path)
-{
-  this->textures.at(0) = IMG_Load(path.c_str());
-}
+// void ImageTexture::loadRight(std::string const &path)
+// {
+//   this->textures.at(0) = IMG_Load(path.c_str());
+// }
 
-void ImageTexture::loadLeft(std::string const &path)
-{
-  this->textures.at(1) = IMG_Load(path.c_str());
-}
+// void ImageTexture::loadLeft(std::string const &path)
+// {
+//   this->textures.at(1) = IMG_Load(path.c_str());
+// }
 
-void ImageTexture::loadUp(std::string const &path)
-{
-  this->textures.at(2) = IMG_Load(path.c_str());
-}
+// void ImageTexture::loadUp(std::string const &path)
+// {
+//   this->textures.at(2) = IMG_Load(path.c_str());
+// }
 
-void ImageTexture::loadDown(std::string const &path)
-{
-  this->textures.at(3) = IMG_Load(path.c_str());
-}
+// void ImageTexture::loadDown(std::string const &path)
+// {
+//   this->textures.at(3) = IMG_Load(path.c_str());
+// }
 
-void ImageTexture::loadFront(std::string const &path)
-{
-  this->textures.at(4) = IMG_Load(path.c_str());
-}
+// void ImageTexture::loadFront(std::string const &path)
+// {
+//   this->textures.at(4) = IMG_Load(path.c_str());
+// }
 
-void ImageTexture::loadBack(std::string const &path)
-{
-  this->textures.at(5) = IMG_Load(path.c_str());
-}
+// void ImageTexture::loadBack(std::string const &path)
+// {
+//   this->textures.at(5) = IMG_Load(path.c_str());
+// }
